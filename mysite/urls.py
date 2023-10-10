81604c9a074445a3d87d2d4c6c7ed6ea2c05abb2
Kaopong Sangkittiyakorn <84172946+kuisskui@users.noreply.github.com>
2023-10-10 23:55:50 +0700

    auth URLs :)

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("audience.urls")),
-    path('admin/', admin.site.urls)
+
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('admin/', admin.site.urls),
 ]
