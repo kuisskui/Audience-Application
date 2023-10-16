2bf165f4a1ad49f9a7083e798de9cb717b3822e3
Kaopong Sangkittiyakorn <84172946+kuisskui@users.noreply.github.com>
2023-10-16 14:59:30 +0700

    done with sub but still can duplicated sub

--- mysite/views.py
+++ mysite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import NewUserForm
 from django.contrib.auth import login
 from django.contrib import messages
+from user_profile.models import UserProfile
 
 
 def register(request):
@@ -9,11 +10,11 @@ def register(request):
         form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
-            user.refresh_from_db()
-            user.userprofile.gender = form.cleaned_data.get("gender")
-            user.userprofile.year = form.cleaned_data.get("year")
-            user.userprofile.country_id = form.cleaned_data.get("country_id")
-            user.save()
+            gender = form.cleaned_data.get("gender")
+            year = form.cleaned_data.get("year")
+            country_id = form.cleaned_data.get("country_id")
+            profile = UserProfile.objects.create(user=user, gender=gender, year=year, country_id=country_id)
+            profile.save()
             login(request, user)
             messages.success(request, "Registration successful.")
             return redirect("audience:dashboard")
