697e93dfe2b1e6150c116e97cf6313b22d83cea0
Kaopong Sangkittiyakorn <84172946+kuisskui@users.noreply.github.com>
2023-10-16 10:48:58 +0700

    add data to the page

--- audience/urls.py
+++ audience/urls.py
@@ -6,8 +6,10 @@ app_name = "audience"
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
     path("sports/", views.sports, name="sports"),
-    path("sport/<int:pk>", views.sport, name="sport"),
+    path("sport/<int:sport_id>", views.sport, name="sport"),
 
     # For testing get data
     path('get_dashboard/', views.get_dashboard, name="get_dashboard"),
+    path('get_sports/', views.get_sports, name="get_sports"),
+    path('get_sport/<int:sport_id>', views.get_sport, name="get_sport"),
 ]
--- audience/views.py
+++ audience/views.py
@@ -11,12 +11,15 @@ def dashboard(request):
 
 
 def sports(request):
-    context = {"page": "sports", "detail": "show all sports without any detail or information."}
+    response = requests.get('http://127.0.0.1:8000/get_sports')
+    context = {"page": "sports", "detail": "show all sports without any detail or information.",
+               "data": response.json()}
     return render(request, "audience/sports.html", context)
 
 
-def sport(request, pk):
-    context = {"page": "sport", "detail": f"show detail on each sport(pk ={pk})."}
+def sport(request, sport_id):
+    response = requests.get(f'http://127.0.0.1:8000/get_sport/{sport_id}')
+    context = {"page": "sport", "detail": f"show detail on each sport(sport_id ={sport_id}).", "data":response.json()}
     return render(request, "audience/sport.html", context)
 
 
@@ -35,4 +38,39 @@ def get_dashboard(request):
         }
     }
     print("return!!")
+    return JsonResponse(data)
+
+
+def get_sports(request):
+    data = {
+        "01": "Atheletics",
+        "02": "Archery",
+        "03": "Artistic Gymnastics",
+        "04": "Artistic Swimming",
+    }
+    return JsonResponse(data)
+
+
+def get_sport(request, sport_id):
+    data = {
+        "sport": sport_id,
+        "sport_name": "Atheletics",
+        "gold": 100,
+        "silver": 100,
+        "bronze": 100,
+        "individual_countries": [
+            {
+                "country_code": "FR",
+                "gold": 10,
+                "silver": 10,
+                "bronze": 10
+            },
+            {
+                "country_code": "US",
+                "gold": 10,
+                "silver": 10,
+                "bronze": 10
+            }
+        ]
+    }
     return JsonResponse(data)
