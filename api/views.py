a1886cedaf1aa097f0f0a4028682085ed64a2a76
Kaopong Sangkittiyakorn <84172946+kuisskui@users.noreply.github.com>
2023-10-16 13:45:32 +0700

    save api

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from audience.models import Sport, Score, Country, Type
+from user_profile.models import UserProfile
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -9,3 +11,28 @@ def webhook(request):
     sport_type = request.Post.get('sport_type')
     participant = request.Post.get('participant')
     sport = Sport.object.get(sport_id=sport_id)
+    # not done
+
+
+def update_audience_info(request):
+    profiles = UserProfile.objects.all()
+    print(profiles[1].year)
+    for profile in profiles:
+        print(profile.user.username, f"gender: {profile.gender}", f"year: {profile.year}", f"country_id: {profile.country_id}", f"sport_id: {profile.sport_ids.all()}")
+    data = {
+        "audience": [
+            {
+                "country": "FR",
+                "sport_ids": ["01", "02", "03"],
+                "gender": "M",
+                "age": 30
+            },
+            {
+                "country": "TH",
+                "sport_ids": ["11", "12"],
+                "gender": "F",
+                "age": 24
+            },
+        ]
+    }
+    return JsonResponse(data)
