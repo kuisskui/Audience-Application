0225936c3174f9a2d59b26bdf7311764bddfae6e
Kaopong Sangkittiyakorn <84172946+kuisskui@users.noreply.github.com>
2023-10-19 14:25:38 +0700

    provide audiences endpoint

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.webhook, name="webhook"),
-    path("update_audience_info/", views.update_audience_info, name="update_audience_info")
+    path("audiences/", views.audiences, name="audiences")
 ]
