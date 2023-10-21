8866394f391f6d54946d4a77e9e0064da91152e5
Kaopong Sangkittiyakorn <84172946+kuisskui@users.noreply.github.com>
2023-10-21 13:29:13 +0700

    set static dir

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,8 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    "mysite/static",
 ]
 
 # Default primary key field type
