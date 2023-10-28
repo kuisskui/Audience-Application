906b9c4c79984258f3920fd381e3072a442045ba
teenteennnii <teennopwijit@gmail.com>
2023-10-28 19:14:27 +0700

    fix credentials risk

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import dotenv_values
 import os
+
+config = dotenv_values(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,11 +69,11 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587 
+EMAIL_HOST = config["EMAIL_HOST"]
+EMAIL_PORT = int(config["EMAIL_PORT"])
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'teennopwijit@gmail.com'
-EMAIL_HOST_PASSWORD = 'wwygrlggdweemazu'
+EMAIL_HOST_USER = config["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = config["EMAIL_HOST_PASSWORD"]
 
 # if DEBUG:
 #     EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
