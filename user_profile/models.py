bf60aaf00ffee2aa1c044100f51f919ddfb12195
Kaopong Sangkittiyakorn <84172946+kuisskui@users.noreply.github.com>
2023-11-02 14:10:14 +0700

    delete debug

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,4 +1,7 @@
+import requests
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 
 
@@ -10,9 +13,20 @@ class UserProfile(models.Model):
     sport_ids = models.TextField(blank=True, null=True)
 
 
-# @receiver(post_save, sender=User)
-# def update_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         print(instance)
-#         UserProfile.objects.create(user=instance)
-#     instance.userprofile.save()
+@receiver(post_save, sender=UserProfile)
+def update_user_profile(sender, instance, created, **kwargs):
+    """Perform when a profile instance is updated or created"""
+    url = f"http://example_domain/audient/update_audient_info"
+    payload = {
+        "id": instance.pk,
+        "country": instance.country,
+        "sport_ids": None if instance.sport_ids is None else instance.sport_ids.split(','),
+        "gender": instance.gender,
+        "age": instance.age
+    }
+    headers = {
+        "Authorization": "Bearer YOUR_ACCESS_TOKEN",
+        "Content-Type": "application/json"
+    }
+    print(payload)
+    # requests.post(url, json=payload, headers=headers)
