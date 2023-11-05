e86625f446ed2cdf64de6e9fd43474edf07a80c0
teenteennnii <teennopwijit@gmail.com>
2023-11-05 18:59:28 +0700

    organize google provider

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,13 +82,8 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
-        'SCOPE': [
-            'profile',
-            'email',
-        ],
-        'AUTH_PARAMS': {
-            'access_type': 'online',
-        }
+        'SCOPE': ['profile','email', ],
+        'AUTH_PARAMS': {'access_type': 'online', }
     }
 }
 
