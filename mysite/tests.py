ef99d5e69fa4c83a00d49ffe3b9929accadf26b5
teenteennnii <teennopwijit@gmail.com>
2023-11-10 10:09:53 +0700

    change to use .env

--- mysite/tests.py
+++ mysite/tests.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 from django.test import TestCase
 from django.contrib.sites.models import Site
 from allauth.socialaccount.models import SocialApp
@@ -7,7 +10,7 @@ from user_profile.models import UserProfile
 from .forms import NewUserForm
 from django.urls import reverse
 
-callback_url = 'https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=1020628582950-uqlvo1ok0sfv8bbe92p6hm06ha687g8a.apps.googleusercontent.com&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Faccounts%2Fgoogle%2Flogin%2Fcallback%2F&scope=profile%20email&response_type=code&state=wo2YqVWsepVX&access_type=online&service=lso&o2v=1&theme=glif&flowName=GeneralOAuthFlow'
+callback_url = os.getenv("GOOGLE_CALLBACK_URL")
 
 class AllAuthTests(TestCase, OAuth2TestsMixin):
     def setUp(self):
@@ -18,8 +21,8 @@ class AllAuthTests(TestCase, OAuth2TestsMixin):
         self.app = SocialApp.objects.create(
             provider="google",
             name="Google",
-            client_id="1020628582950-uqlvo1ok0sfv8bbe92p6hm06ha687g8a.apps.googleusercontent.com",
-            secret="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",
+            client_id=os.getenv("GOOGLE_CLIENT_ID"),
+            secret=os.getenv("GOOGLE_SECRET_KEY"),
         )
         self.app.sites.add(self.site)
         self.app.callback_url = self.client.get(callback_url)
