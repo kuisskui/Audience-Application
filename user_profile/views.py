19e923a18c0c05f0f488dcf47ceace63f36cbd49
Tharunthorn Musik <tharunthornmusik1446@gmail.com>
2023-11-21 18:46:02 +0700

    profile and sport sub and unsub

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
+import requests
 
 
 # Create your views here.
 @login_required
 def profile(request):
+    api_key = '02e2cdc6ac5d17a2bb67824c91f51ac55ce46465133f92233e3daa552120bcb3'
+    sport_url = 'https://referite-6538ffaf77b0.herokuapp.com/api/schedule/sport'
+    headers = {'Accept': 'application/json', 'Authorization': api_key}
+
     try:
+        data = requests.get(sport_url, headers=headers).json()
         userprofile = UserProfile.objects.get(user=request.user)
     except UserProfile.DoesNotExist:
         # If the user profile does not exist, redirect to complete-registration
@@ -14,5 +20,6 @@ def profile(request):
 
     sport_ids = userprofile.sport_ids
     sport_ids = [] if sport_ids is None else sport_ids.split(',')
-    context = {"page": "profile", "detail": "show user profile", "userprofile": userprofile, "sport_ids": sport_ids}
+    context = {"page": "profile", "detail": "show user profile", "userprofile": userprofile, "sport_ids": sport_ids,
+               "sports": data}
     return render(request, "user_profile/profile.html", context)
